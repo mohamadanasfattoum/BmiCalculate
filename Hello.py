afe8479f435cf6ed65c57a87e67db99494f71937
Mohamad anas fattoum <samaanas30@gmail.com>
2024-08-17 12:46:53 +0200

    show Your bmi

--- Hello.py
+++ Hello.py
@@ -17,3 +17,18 @@ def Hi():
 # Hello name von code
 # app name von app
 # reload, damit die Anwendung weiterhin funktioniert
+
+# -----------------
+
+# تحديد المسار الرأيسي
+# Definieren den Hauptpfad
+@app.get("/calculate_bmi")
+
+def calculate_bmi(weight: float, height:float):
+    bmi = weight / (height ** 2)
+
+    return {'Your bmi': bmi}
+
+# http://127.0.0.1:8000/calculate_bmi?weight=80&height=1.7
+# weight=in kg &height= in m
+# outpot: {"Your bmi":27.68166089965398}
